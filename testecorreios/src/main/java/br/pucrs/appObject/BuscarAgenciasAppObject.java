10ccea57ead3fb7603b9c65f5a2c80e20de743f9
Adriano <adrianochaves93@gmail.com>
2018-05-30 15:00:16 -0300

    adicionado mapeamento

--- testecorreios/src/main/java/br/pucrs/appObject/BuscarAgenciasAppObject.java
+++ testecorreios/src/main/java/br/pucrs/appObject/BuscarAgenciasAppObject.java
@@ -16,6 +16,21 @@ public class BuscarAgenciasAppObject {
 		
 		}
 		
+		public WebElement getServicoRadialButton() {
+			return this.driver.findElement(By.xpath("//*[@id=\"formBuscaAgencia\"]/div/div/div[1]/span[3]/label"));
+		}
+		
+		public WebElement getEstadoComboBox() {
+			return this.driver.findElement(By.id("estadoAgencia"));
+		}
+		
+		public WebElement getMunicipioComboBox() {
+			return this.driver.findElement(By.id("municipioAgencia"));
+		}
+		ptubli WebElement getBairroComboBox() {
+			return this.driver.findElement(By.id("bairroAgencia"));
+		}
+		
 		public WebElement buscarAgenciaPorProximidadePesquisarButton() {
 			return this.driver.findElement(By.id("submit"));
 		}
