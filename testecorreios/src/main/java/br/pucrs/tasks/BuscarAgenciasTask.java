ecd72af56f133151551fc3811f46465091fbc0de
Fabiano M Dutra <fmbdutra@gmail.com>
2018-06-01 13:52:00 -0300

    excluida task Busca por prox. e movido p/BuscarAgencias

--- testecorreios/src/main/java/br/pucrs/tasks/BuscarAgenciasTask.java
+++ testecorreios/src/main/java/br/pucrs/tasks/BuscarAgenciasTask.java
@@ -1,6 +1,7 @@
 package br.pucrs.tasks;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import br.pucrs.appObject.BuscarAgenciasAppObject;
 import br.pucrs.appObject.HomeAppObject;
@@ -19,6 +20,32 @@ public class BuscarAgenciasTask {
 
 	public void apertarBotaoBuscarAgencia() {
 		this.homeAppObject.getBuscarAgencia().click();
+	}	
+	
+	public void selecionarProximidadeRadialButton () {
+		this.buscarAgenciasAppObject.getProximidadeRadialButton().click();
+	}
+
+	public void digitarLocalizacao(String endereco) {
+		this.buscarAgenciasAppObject.getEnderecoTextField().sendKeys(endereco);		
+	}
+	
+	public void pesquisarLocalizacao() {
+		this.buscarAgenciasAppObject.getPesquisarButton().click();
+	}
+	
+	public boolean verificaCarregamentodoMapa() {
+		return this.buscarAgenciasAppObject.getCarregamentoImg().isDisplayed();			
+	}
+	
+	public void rolarPaginaParaVerificarMapa() {
+		
+					
+		for(int x=1;x<=10;x++)
+		{
+			this.buscarAgenciasAppObject.getPesquisarButton().sendKeys(Keys.ARROW_DOWN);
+		} 
+	
 	}
 
 	public void selecionarEstadoComboBox(String estado) {
