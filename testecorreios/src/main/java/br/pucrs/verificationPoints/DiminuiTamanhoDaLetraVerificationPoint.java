9847b5c4cd37eef08bcc0eef7b92a135a74d8a52
Fabiano M. Dutra <38920482+fmbdutra@users.noreply.github.com>
2018-12-05 14:18:21 -0200

    Ajuste de Verification Points e Test Cases

--- testecorreios/src/main/java/br/pucrs/verificationPoints/DiminuiTamanhoDaLetraVerificationPoint.java
+++ testecorreios/src/main/java/br/pucrs/verificationPoints/DiminuiTamanhoDaLetraVerificationPoint.java
@@ -1,28 +1,27 @@
 package br.pucrs.verificationPoints;
 
 import static org.junit.Assert.assertEquals;
-
 import org.openqa.selenium.WebDriver;
-
 import com.aventstack.extentreports.Status;
 
 import br.pucrs.framework.Report;
 import br.pucrs.framework.Screenshot;
-import br.pucrs.tasks.DiminuiTamanhoDaLetraTask;
+
+import br.pucrs.appObject.HomeAppObject;
 
 public class DiminuiTamanhoDaLetraVerificationPoint {
 	private WebDriver driver;
-
-	private DiminuiTamanhoDaLetraTask diminuiTamanhoDaLetraTask;
-
+	
+	private HomeAppObject homeAppObject; 
+	
 	public DiminuiTamanhoDaLetraVerificationPoint(WebDriver driver) {
 		this.driver = driver;
-		this.diminuiTamanhoDaLetraTask = new DiminuiTamanhoDaLetraTask(driver);
+		this.homeAppObject = new HomeAppObject(driver);
 	}
 
 	public void verificarReducaoDaFonte() {
-		String atual = this.diminuiTamanhoDaLetraTask.apertarBotaoDiminuir();
-		Report.log(Status.INFO, "O Botão A- Foi Pressionado");
+		String atual = homeAppObject.getFonteDoTextoHtml().getCssValue("font-size");
+	
 		if (atual == "12px") {
 			Report.log(Status.PASS, "O Tamanho Da Fonte Do Texto Foi Alterado Conforme O Esperado.",
 					Screenshot.capture(driver));
