6689a6f7243bbc130640ad9fb3ebc4d72bae25e8
Fabiano M. Dutra <38920482+fmbdutra@users.noreply.github.com>
2018-12-05 14:17:11 -0200

    Ajuste Verification Points

--- testecorreios/src/main/java/br/pucrs/testCase/DiminuiTamanhoDaLetraTestCase.java
+++ testecorreios/src/main/java/br/pucrs/testCase/DiminuiTamanhoDaLetraTestCase.java
@@ -10,11 +10,13 @@ import com.aventstack.extentreports.Status;
 import br.pucrs.framework.Driver;
 import br.pucrs.framework.Report;
 import br.pucrs.framework.Screenshot;
+import br.pucrs.tasks.DiminuiTamanhoDaLetraTask;
 import br.pucrs.verificationPoints.DiminuiTamanhoDaLetraVerificationPoint;
 
 public class DiminuiTamanhoDaLetraTestCase {
 	private WebDriver driver;
 	private DiminuiTamanhoDaLetraVerificationPoint diminuiTamanhoDaLetraVerificationPoint;
+	private DiminuiTamanhoDaLetraTask diminuiTamanhoDaLetraTask;
 
 	@Before
 	public void setUp() {
@@ -26,12 +28,17 @@ public class DiminuiTamanhoDaLetraTestCase {
 		driver.manage().window().maximize();
 
 		this.diminuiTamanhoDaLetraVerificationPoint = new DiminuiTamanhoDaLetraVerificationPoint(driver);
+		this.diminuiTamanhoDaLetraTask = new DiminuiTamanhoDaLetraTask(driver);
 
 		Report.log(Status.INFO, "A página foi carregada", Screenshot.capture(driver));
 	}
 
 	@Test
 	public void main() {
+		
+		diminuiTamanhoDaLetraTask.apertarBotaoDiminuir();		
+		Report.log(Status.INFO, "O Botão A- Foi Pressionado");
+		
 		this.diminuiTamanhoDaLetraVerificationPoint.verificarReducaoDaFonte();
 	}
 
