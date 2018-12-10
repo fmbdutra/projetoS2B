a727ead442b126b4996f5245f5d05415e30a71fb
Fabiano Dutra <fmbdutra@gmail.com>
2018-12-10 09:40:22 -0200

    ajuste BuscarAgenciaPorProximidade verificationpoint e testcase

--- testecorreios/src/main/java/br/pucrs/testCase/BuscarAgenciaPorProximidadeTestCase.java
+++ testecorreios/src/main/java/br/pucrs/testCase/BuscarAgenciaPorProximidadeTestCase.java
@@ -5,13 +5,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
+import com.aventstack.extentreports.Status;
+
 import br.pucrs.framework.Driver;
 import br.pucrs.framework.Report;
+import br.pucrs.tasks.BuscarAgenciasTask;
 import br.pucrs.verificationPoints.BuscarAgenciaPorProximidadeVerificationPoint;
 
 public class BuscarAgenciaPorProximidadeTestCase {
 	private WebDriver driver;
 	private BuscarAgenciaPorProximidadeVerificationPoint buscarAgenciaPorProximidadeVerificationPoint;
+	private BuscarAgenciasTask buscarAgenciasTask;
+	 
 
 	@Before
 	public void setUp() {
@@ -23,11 +28,35 @@ public class BuscarAgenciaPorProximidadeTestCase {
 		driver.manage().window().maximize();
 
 		this.buscarAgenciaPorProximidadeVerificationPoint = new BuscarAgenciaPorProximidadeVerificationPoint(driver);
-
+		this.buscarAgenciasTask = new BuscarAgenciasTask(driver);
 	}
 
 	@Test
 	public void main() throws InterruptedException {
+		
+		String enderecoBuscado = "Rodovia RS 118, 11800 - Maria Regina, Alvorada - RS, 94834-670";
+
+		this.buscarAgenciasTask.apertarBotaoBuscarAgencia();
+		Report.log(Status.INFO, "Entrou Na Página Inicial dos Correios");
+
+		this.buscarAgenciasTask.selecionarProximidade();
+		Report.log(Status.INFO, "Entrou na Página De 'Busca de Agências'");
+
+		Report.log(Status.INFO, "Buscar Agências Por Proximidade Foi Selecionado");
+
+		this.buscarAgenciasTask.digitarLocalizacao(enderecoBuscado);
+		Report.log(Status.INFO, "Buscou Endereço " + enderecoBuscado);
+
+		this.buscarAgenciasTask.pesquisarLocalizacao();
+		Report.log(Status.INFO, "O Botão 'Pesquisar' foi Pressionado");
+
+		while (this.buscarAgenciasTask.verificaCarregamentodoMapa() == false) {
+			Thread.sleep(2000);
+		}
+
+		Thread.sleep(2000);
+
+		this.buscarAgenciasTask.rolarPaginaParaVerificarResultado();
 
 		this.buscarAgenciaPorProximidadeVerificationPoint.checarEnderecoDaAgenciaNoMapa();
 	}
--- testecorreios/src/main/java/br/pucrs/verificationPoints/BuscarAgenciaPorProximidadeVerificationPoint.java
+++ testecorreios/src/main/java/br/pucrs/verificationPoints/BuscarAgenciaPorProximidadeVerificationPoint.java
@@ -12,44 +12,15 @@ public class BuscarAgenciaPorProximidadeVerificationPoint {
 
 	private WebDriver driver;
 
-	private BuscarAgenciasTask buscarAgenciasTask;
-
 	public BuscarAgenciaPorProximidadeVerificationPoint(WebDriver driver) {
 		this.driver = driver;
-		this.buscarAgenciasTask = new BuscarAgenciasTask(driver);
 	}
 
 	public void checarEnderecoDaAgenciaNoMapa() throws InterruptedException {
 
-		String enderecoBuscado = "Rodovia RS 118, 11800 - Maria Regina, Alvorada - RS, 94834-670";
-
-		this.buscarAgenciasTask.apertarBotaoBuscarAgencia();
-		Report.log(Status.INFO, "Entrou Na Página Inicial dos Correios");
-
-		this.buscarAgenciasTask.selecionarProximidade();
-		Report.log(Status.INFO, "Entrou na Página De 'Busca de Agências'");
-
-		Report.log(Status.INFO, "Buscar Agências Por Proximidade Foi Selecionado");
-
-		this.buscarAgenciasTask.digitarLocalizacao(enderecoBuscado);
-		Report.log(Status.INFO, "Buscou Endereço " + enderecoBuscado);
-
-		this.buscarAgenciasTask.pesquisarLocalizacao();
-		Report.log(Status.INFO, "O Botão 'Pesquisar' foi Pressionado");
-
-		while (this.buscarAgenciasTask.verificaCarregamentodoMapa() == false) {
-
-			Thread.sleep(2000);
-		}
-
-		Thread.sleep(2000);
-
-		this.buscarAgenciasTask.rolarPaginaParaVerificarResultado();
-
 		Report.log(Status.INFO,
 				"O Mapa Deve Ter Marcação Exibindo Localização Da Agência Mais Próxima do Endereço Pesquisado",
 				Screenshot.capture(driver));
-
 	}
 
 }
